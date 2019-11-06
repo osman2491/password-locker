886d8b50b691e89f3a569cdcc72d38a7635d5eb6
osman67239121@gmail.com <osman67239121@gmail.com>
2019-11-06 17:49:37 +0300

    added a method that saves and delets user also tested it

--- user.py
+++ user.py
@@ -1,17 +1,23 @@
 class User:
+    
 
-    user_list = []
+    user_list = [] 
 
     def __init__(self,first_name,last_name,password):
+
 
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
-
+ 
     def save_user(self):
 
 
-        self.user_list.append(self)
+        User.user_list.append(self)
+
+    def delete_user(self):
+
+
+        User.user_list.remove(self)
 
     
-
--- user_credential_test.py
+++ user_credential_test.py
@@ -1,30 +1,44 @@
-import unittest
-from user import User
+import unittest 
+from user import User 
 
-class testUser(unittest.TestCase):
+class TestContact(unittest.TestCase):
+
 
     def tearDown(self):
+            
+            User.user_list = []
 
-
-        User.user_list = []
 
     def setUp(self):
+        
+        self.new_user = User("osman","mohamed","P@#97")
 
-        self.new_user = User("osman","mohamed","4@PO3*")
 
-    def test__init__(self):
+    def test_init(self):
 
         self.assertEqual(self.new_user.first_name,"osman")
         self.assertEqual(self.new_user.last_name,"mohamed")
-        self.assertEqual(self.new_user.password,"4@PO3*")
-
-    def save_user(self):
+        self.assertEqual(self.new_user.password,"P@#97")
 
 
-        self.new_user.save_user()
+    def test_save_user(self):
+        
+        self.new_user.save_user() 
         self.assertEqual(len(User.user_list),1)
 
-    
+
+    def test_delete_user(self):
+            
+            self.new_user.save_user()
+            test_user = User("osman","mohamed","P@#97") 
+            test_user.save_user()
+
+            self.new_user.delete_user()
+            self.assertEqual(len(User.user_list),1)
+
+
 
 if __name__ == '__main__':
-    unittest.main()        
+    unittest.main()
+
+
