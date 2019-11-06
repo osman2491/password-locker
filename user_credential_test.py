4e2862b359d185c676b4fdc30e2716605e9d8280
osman67239121@gmail.com <osman67239121@gmail.com>
2019-11-06 14:58:03 +0300

    created contact object and tested it

---
+++ user_credential_test.py
@@ -0,0 +1,26 @@
+import unittest
+from user import User
+
+class testUser(unittest.TestCase):
+
+    def setUp(self):
+
+        self.new_user = User("osman","mohamed","4@PO3*")
+
+    def test__init__(self):
+
+        self.assertEqual(self.new_user.first_name,"osman")
+        self.assertEqual(self.new_user.last_name,"mohamed")
+        self.assertEqual(self.new_user.password,"4@PO3*")
+
+
+    def save_user(self):
+
+
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+
+
+if __name__ == '__main__':
+    unittest.main()        
