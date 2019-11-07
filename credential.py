64d5405d97be3f1cfed7a5a4f784f024fd8a012f
osman67239121@gmail.com <osman67239121@gmail.com>
2019-11-07 11:16:48 +0300

    created an empty file for testing credentials

---
+++ credential.py
@@ -0,0 +1,52 @@
+import random
+import string
+class Credential:
+
+    app_details = []
+
+    def __init__(self,app,app_password):
+
+        self.app = app
+        self.app_password = app_password
+
+    def save_app(self):
+
+        self.app_details.append(self)
+
+    def delete_app(self):
+
+        self.app_details.remove(self)
+
+    @classmethod
+    def find_app(cls,app):
+
+
+        for Credential in cls.app_details:
+
+            if Credential.app == app:
+
+                return True
+
+    @classmethod
+    def app_exists(cls,app):
+
+        for Credential in cls.app_details:
+            if Credential.app == app:
+                return True
+
+        return False
+
+
+    @classmethod
+    def gen_password(self):
+
+
+        char = string.ascii_uppercase + string.ascii_lowercase
+        gen_password = ''.join(random.choice(char) for i in range(0, 9))
+        return gen_password
+
+    @classmethod
+    def display_app(cls,app):
+
+        return cls.app_details
+             
